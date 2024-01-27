f324039684307c3312e7033b712f17a7deaebf9b
unknown <anastasija@elfak.rs>
2024-01-27 20:00:10 +0100

    Testovi API

---
+++ Projekat/PlaywrightTests/TestsAPI/RegistracijeTestsAPI.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests.TestsAPI
+{
+    internal class RegistracijeTestsAPI : PlaywrightTest
+    {
+        private IAPIRequestContext Request;
+
+        [SetUp]
+        public async Task SetUpAPITesting()
+        {
+            var headers = new Dictionary<string, string>
+            {
+                { "Accept", "application/json" }
+            };
+
+            Request = await Playwright.APIRequest.NewContextAsync(new()
+            {
+                BaseURL = "https://localhost:7193",
+                ExtraHTTPHeaders = headers,
+                IgnoreHTTPSErrors = true
+            });
+        }
+    }
+}
